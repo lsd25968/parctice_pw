5f7ae9ecfbd7fb7f739da60b2ee6203595750b62
Cookie Liu <ps259685@gmail.com>
2022-02-07 22:41:13 +0800

    version1.1

--- password.py
+++ password.py
@@ -1,8 +1,7 @@
 true_pw = 'a123456'
-pw = ''
 times = 2
 
-while pw != true_pw:
+while time > -1:
 	pw = input('請輸入密碼: ')
 	if pw == true_pw:
 		print('登入成功')
@@ -10,5 +9,3 @@ while pw != true_pw:
 	else:
 		print('密碼錯誤, 你還有',times ,'次輸入機會')
 		times = times - 1
-		if times == -1:
-			break
